c29f650d2d1968dad2f09e14a9022de4975fe8f2
FullyAngozi <ochuks33@yahoo.com>
2023-03-10 15:07:37 -0500

    mo

---
+++ client/src/fireabase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6_XLBQvUP1hxAN1YHm-fY-sXo9ZAuwdo",
+  authDomain: "e-commerce-57f09.firebaseapp.com",
+  projectId: "e-commerce-57f09",
+  storageBucket: "e-commerce-57f09.appspot.com",
+  messagingSenderId: "89677469464",
+  appId: "1:89677469464:web:d47426c7e7f75c870ef06d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
